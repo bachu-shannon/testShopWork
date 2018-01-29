cbee6ed88b526ac85346c7c8e5ef199b1f1bfb27
bachu_shannon <ba4yrik@gmail.com>
2018-01-29 14:31:48 +0200

    first commit

--- src/actions/UsersAction.js
+++ src/actions/UsersAction.js
@@ -1,20 +1,20 @@
 import {
-    GET_USERS_REQUEST,
+	GET_USERS_REQUEST,
 	UPDATE_USERS_REQUEST
 } from '../constants/actionTypes';
 
 export function getDataRequest(data) {
-    return {
-        type: GET_USERS_REQUEST,
-        payload: {
-            data,
-            fetched: true
-        }
-    }
+	return {
+		type: GET_USERS_REQUEST,
+		payload: {
+			data,
+			fetched: true
+		}
+	}
 }
 
 export function updateDataRequest(data) {
-    console.log(data);
+	console.log(data);
 	return {
 		type: UPDATE_USERS_REQUEST,
 		payload: {
