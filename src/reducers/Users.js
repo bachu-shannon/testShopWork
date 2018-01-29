4f998be54417c85fa1faa91ddfb7cff398592ddf
bachu_shannon <ba4yrik@gmail.com>
2018-01-29 14:33:39 +0200

    first commit

--- src/reducers/Users.js
+++ src/reducers/Users.js
@@ -4,23 +4,23 @@ import {
 } from '../constants/actionTypes';
 
 const initialState = {
-    data: [],
-    fetched: false,
+	data: [],
+	fetched: false,
 };
 
 export default function getUsers(state = initialState, action) {
 	switch (action.type) {
-        case GET_USERS_REQUEST:
-            return {
-                ...state,
-                ...action.payload
-            };
-        case UPDATE_USERS_REQUEST:
+		case GET_USERS_REQUEST:
+			return {
+				...state,
+				...action.payload
+			};
+		case UPDATE_USERS_REQUEST:
 			return {
 				...state, ...{data: action.payload.data}
 			};
 
 		default:
-            return state;
-    }
+			return state;
+	}
 }
