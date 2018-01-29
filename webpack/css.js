9a0048e04e2a3433e1f02a44a47f79241935d230
bachu_shannon <ba4yrik@gmail.com>
2018-01-29 14:37:22 +0200

    first commit

--- webpack/css.js
+++ webpack/css.js
@@ -1,40 +1,40 @@
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = function () {
-	return {
-		module: {
-			rules: [
-				{
-					test: /\.css$/,
-					use: ExtractTextPlugin.extract({
-						fallback: 'style-loader',
-						use: [
-							{
-								loader: 'css-loader',
-								options: {
-									importLoaders: 1,
-									sourceMap: true
-								}
-							},
-						]
-					})
-				},
-				{
-					test: /\.(png|svg|jpg|gif)$/,
-					use: [
-						'file-loader'
-					]
-				},
-				{
-					test: /\.(woff|woff2|eot|ttf|otf)$/,
-					use: [
-						'file-loader'
-					]
-				}
-			],
-		},
-		plugins: [
-			new ExtractTextPlugin('styles.[hash].css'),
-		],
-	};
+return {
+module: {
+rules: [
+{
+test: /\.css$/,
+use: ExtractTextPlugin.extract({
+fallback: 'style-loader',
+use: [
+{
+loader: 'css-loader',
+options: {
+importLoaders: 1,
+sourceMap: true
+}
+},
+]
+})
+},
+{
+test: /\.(png|svg|jpg|gif)$/,
+use: [
+'file-loader'
+]
+},
+{
+test: /\.(woff|woff2|eot|ttf|otf)$/,
+use: [
+'file-loader'
+]
+}
+],
+},
+plugins: [
+new ExtractTextPlugin('styles.[hash].css'),
+],
+};
 };
